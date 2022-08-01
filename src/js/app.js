466261c7ee48f08058f1b93632369275823f6257
Legebrind <moyi88@gmail.com>
2022-08-01 17:40:40 +0000

    Fixed links

--- src/js/app.js
+++ src/js/app.js
@@ -41,18 +41,24 @@ function render(variables = {}) {
     variables.country == null ? "USA" : variables.country
   }</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy">${
-              variables.twitter == null ? "" : variables.twitter
-            }<i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy">${
-              variables.github == "alesanchezr" ? "" : variables.github
-            }<i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy">${
-              variables.linkedin == null ? "" : variables.linkedin
-            }<i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy">${
-              variables.instagram == null ? "" : variables.instagram
-            }<i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "4geeksacademy" : variables.twitter
+            }">
+            <i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github == null ? "4geeksacademy" : variables.github
+            }">
+            <i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin == null ? "4geeksacademy" : variables.linkedin
+            }">
+            <i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null
+                ? "4geeksacademy"
+                : variables.instagram
+            }">
+            <i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
